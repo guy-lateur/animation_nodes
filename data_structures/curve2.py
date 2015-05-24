2f29f6deb549aec88bc21466963058e157ae3a2b
guy lateur <guy.lateur@telenet.be>
2015-05-24 13:24:17 +0200

    removed matrix from curve & curve from spline
    + refactored spline some more

--- data_structures/curve2.py
+++ data_structures/curve2.py
@@ -4,20 +4,18 @@ from . import spline
 from spline import Spline
 
 supportedSplineTypes = ["BEZIER", "NURBS", "POLY"]
-identityMatrix = Matrix.Identity(4)
 
 class Curve:
-    def __init__(self, worldMatrix = identityMatrix):
-        self.worldMatrix = worldMatrix
+    def __init__(self):
         self.splines = []
         
     @staticmethod
     def fromBlenderCurve(blenderCurve):
-        rvCurve = Curve(blenderCurve.matrix_world)
+        rvCurve = Curve()
         
         for blenderSpline in blenderCurve.data.splines:
             if blenderSpline.type in supportedSplineTypes: 
-                rvCurve.splines.append(Spline.fromBlenderSpline(rvCurve, blenderSpline))
+                rvCurve.splines.append(Spline.fromBlenderSpline(blenderCurve, blenderSpline))
         
         return rvCurve
         
--- data_structures/spline.py
+++ data_structures/spline.py
@@ -4,20 +4,19 @@ defaultNurbsOrder = 4
 
 
 class Spline:
-    def __init__(self, curve, isCyclic = False):
-        self.curve = curve
+    def __init__(self, isCyclic = False):
         self.isCyclic = isCyclic
         
     @staticmethod
-    def fromBlenderSpline(curve, blenderSpline):
-        if blenderSpline.type = "BEZIER": return BezierSpline.fromBlenderSpline(curve, blenderSpline)
-        if blenderSpline.type = "NURBS": return NurbsSpline.fromBlenderSpline(curve, blenderSpline)
-        if blenderSpline.type = "POLY": return PolySpline.fromBlenderSpline(curve, blenderSpline)
+    def fromBlenderSpline(blenderCurve, blenderSpline):
+        if blenderSpline.type = "BEZIER": return BezierSpline.fromBlenderSpline(blenderCurve, blenderSpline)
+        if blenderSpline.type = "NURBS": return NurbsSpline.fromBlenderSpline(blenderCurve, blenderSpline)
+        if blenderSpline.type = "POLY": return PolySpline.fromBlenderSpline(blenderCurve, blenderSpline)
         
         return None
         
-    # Propeties every subclass should have:
-    #######################################
+    # Propeties every subclass should have/override:
+    ################################################
     @property
     def type(self):
         # not sure if we should raise an exception here, ATM -- see dev general spline socket
@@ -28,25 +27,34 @@ class Spline:
         # not sure what this is meant for
         raise NotImplementedError("'hasLength' property not implemented")
         
-    # Functions every subclass should have:
-    #######################################
+    # Functions every subclass should have/override:
+    ################################################
     def copy(self):
         raise NotImplementedError("'copy' function not implemented")
+        
+    def transform(self, matrix):
+        raise NotImplementedError("'transform' function not implemented")
         
     def evaluate(self, parameter):
         raise NotImplementedError("'evaluate' function not implemented")
         
     def evaluateTangent(self, parameter):
         raise NotImplementedError("'evaluateTangent' function not implemented")
-        
-    def project(self, coordinates, extend = False):
+    
+    # returns parameter
+    def project(self, point):
         raise NotImplementedError("'project' function not implemented")
         
-    def getLength(self, nrSamples):
-        raise NotImplementedError("'getLength' function not implemented")
+    # returns point & tangent
+    def projectExtended(self, point):
+        raise NotImplementedError("'projectExtended' function not implemented")
+    
+    # can't have samplesPerSegment here, because that would be specific to bezier
+    def calculateLength(self, nrSamples):
+        raise NotImplementedError("'calculateLength' function not implemented")
             
     # get multiple samples
-    #############################
+    ######################
              
     def getSamples(self, nrSamples):
         return self.sampleFunction(self.evaluate, nrSamples)
@@ -60,4 +68,4 @@ class Spline:
             samples.append(function(i / (nrSamples - 1)))
         samples.append(function(1))
         return samples
-
+    
